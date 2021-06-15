e7cea3746831bf59583be2c41a26053eabce81f8
Kronifer <44979306+Kronifer@users.noreply.github.com>
2021-06-15 08:52:07 -0500

    Work on token code

--- catcord/__main__.py
+++ catcord/__main__.py
@@ -1,3 +1,6 @@
+import hashlib
+from uuid import uuid4
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +8,9 @@ app = FastAPI()
 
 @app.get("/")
 async def home():
-    return {"message": "Hello, World"}
+    return {"message": "Send a POST request to /token to generate a token."}
+
+@app.post("/token")
+async def token():
+    token = str(uuid4())
+    tokenhash = hashlib.sha256(bytes(token)).hexdigest()
