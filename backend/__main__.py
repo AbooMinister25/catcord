04c89d2ed015853747336437a6db701768c95992
AbooMinister25 <aboominister@gmail.com>
2021-06-16 14:38:31 -0400

    Fixed linting so that backend is linted with flake8

--- backend/__main__.py
+++ backend/__main__.py
@@ -10,6 +10,7 @@ import time
 
 app = FastAPI()
 
+
 class UserCreateBody(BaseModel):
     username: str
     password: str
@@ -17,6 +18,7 @@ class UserCreateBody(BaseModel):
 
 class NewServerBody(BaseModel):
     name: str
+
 
 class NewMessageBody(BaseModel):
     server_id: str
@@ -31,31 +33,41 @@ async def home():
 @app.post("/token")
 async def token(userinfo: UserCreateBody):
     token = str(uuid4())
-    tokenhash = str(hashlib.sha256(bytes(token, encoding='utf8')).hexdigest())
+    tokenhash = str(hashlib.sha256(bytes(token, encoding="utf8")).hexdigest())
     password = userinfo.password
-    passhash = str(hashlib.sha256(bytes(password, encoding='utf8')).hexdigest())
+    passhash = str(hashlib.sha256(bytes(password, encoding="utf8")).hexdigest())
     conn = await tasks.get_db()
-    await tasks.new_user(conn, str(actions.gensnowflake()), tokenhash, userinfo.username, passhash)
+    await tasks.new_user(
+        conn, str(actions.gensnowflake()), tokenhash, userinfo.username, passhash
+    )
     await conn.close()
     return {"token": token}
 
+
 @app.post("/new_server")
-async def new_server(response: Response, serverinfo: NewServerBody, Auth: str = Header(None)):
+async def new_server(
+    response: Response, serverinfo: NewServerBody, Auth: str = Header(None)
+):
     if Auth is None:
         response.status_code = 403
         return {"error": "No token supplied. Please submit a token."}
-    tokenhash = hashlib.sha256(bytes(Auth, encoding='utf8')).hexdigest()
+    tokenhash = hashlib.sha256(bytes(Auth, encoding="utf8")).hexdigest()
     conn = await tasks.get_db()
     userdata = await conn.fetchrow(f"SELECT * FROM USERS WHERE TOKEN='{tokenhash}';")
     if userdata is None:
         response.status_code = 403
-        return {"error": "Token supplied is invalid. Please correct your token or get one by sending a post request to /token ."}
+        return {
+            "error": "Token supplied is invalid. Please correct your token or get one by sending a post request to /token ."
+        }
     serverid = str(actions.gensnowflake())
     await tasks.new_server(conn, serverid, userdata[0], serverinfo.name)
     return {"server_id": serverid}
 
+
 @app.post("/new_message")
-async def new_message(response: Response, messageinfo: NewMessageBody, Auth: str = Header(None)):
+async def new_message(
+    response: Response, messageinfo: NewMessageBody, Auth: str = Header(None)
+):
     if Auth is None:
         response.status_code = 403
         return {"error": "No token supplied. Please submit a token."}
@@ -64,34 +76,45 @@ async def new_message(response: Response, messageinfo: NewMessageBody, Auth: str
     userdata = await conn.fetchrow(f"SELECT * FROM USERS WHERE TOKEN='{tokenhash}';")
     if userdata is None:
         response.status_code = 403
-        return {"error": "Token supplied is invalid. Please correct your token or get one by sending a post request to /token ."}
+        return {
+            "error": "Token supplied is invalid. Please correct your token or get one by sending a post request to /token ."
+        }
     messageid = str(actions.gensnowflake())
-    await tasks.new_message(conn, messageid, time.time_ns(), userdata[0], messageinfo.server_id, messageinfo.message_content)
+    await tasks.new_message(
+        conn,
+        messageid,
+        time.time_ns(),
+        userdata[0],
+        messageinfo.server_id,
+        messageinfo.message_content,
+    )
     return {"message_id": messageid}
 
+
 @app.get("/get_messages")
-async def get_messages(response: Response, server_id: str = None, Auth: str = Header(None)):
+async def get_messages(
+    response: Response, server_id: str = None, Auth: str = Header(None)
+):
     if Auth is None:
         return {"error": "No token supplied. Please submit a token."}
     conn = await tasks.get_db()
     tokenhash = actions.gentokenhash(Auth)
     userdata = await conn.fetchrow(f"SELECT * FROM USERS WHERE TOKEN='{tokenhash}'")
     if userdata is None:
-        return {"error": "Token supplied is invalid. Pleasse correct your token or get one by sending a post request to /tokens ."}
+        return {
+            "error": "Token supplied is invalid. Pleasse correct your token or get one by sending a post request to /tokens ."
+        }
     messages = await tasks.get_messages(conn, server_id)
     messagelist = []
     for element in messages:
         message = {}
-        message['id'] = element[0]
-        message['timestamp'] = element[1]
-        message['sender'] = element[2]
-        sender_name = await conn.fetchrow(f"SELECT USERNAME FROM USERS WHERE ID='{element[2]}'")
-        message['sender_name'] = sender_name['username']
-        message['content'] = element[4]
+        message["id"] = element[0]
+        message["timestamp"] = element[1]
+        message["sender"] = element[2]
+        sender_name = await conn.fetchrow(
+            f"SELECT USERNAME FROM USERS WHERE ID='{element[2]}'"
+        )
+        message["sender_name"] = sender_name["username"]
+        message["content"] = element[4]
         messagelist.append(message)
     return messagelist
-
-
-
-
-
--- backend/db/tasks.py
+++ backend/db/tasks.py
@@ -15,37 +15,64 @@ async def get_db() -> typing.Optional[asyncpg.Connection]:
         logger.error("Exception occured while attempting to connect to database")
         logger.error(e)
 
-async def new_user(connection: asyncpg.Connection, id: str, tokenhash: str, username: str, passhash: str):
+
+async def new_user(
+    connection: asyncpg.Connection,
+    id: str,
+    tokenhash: str,
+    username: str,
+    passhash: str,
+):
     logger.info("Attempting to add a new user to DB")
     try:
-        await connection.execute(f"INSERT INTO USERS VALUES({id}, '{tokenhash}', '{username}', '{passhash}');")
+        await connection.execute(
+            f"INSERT INTO USERS VALUES({id}, '{tokenhash}', '{username}', '{passhash}');"
+        )
         logger.info("Successfully added a new user to DB")
     except Exception as e:
         logger.error("Exception occured while trying to add a user to database")
         logger.error(e)
 
-async def new_server(connection: asyncpg.Connection, server_id: str, owner_id: str, server_name: str):
+
+async def new_server(
+    connection: asyncpg.Connection, server_id: str, owner_id: str, server_name: str
+):
     logger.info("Attempting to add a server to DB")
     try:
-        await connection.execute(f"INSERT INTO SERVERS VALUES('{server_id}', '{owner_id}', '{server_name}');")
+        await connection.execute(
+            f"INSERT INTO SERVERS VALUES('{server_id}', '{owner_id}', '{server_name}');"
+        )
         logger.info("Successfully added a server to DB")
     except Exception as e:
         logger.error("Exception occured while trying to add a server to database")
         logger.error(e)
 
-async def new_message(connection: asyncpg.Connection, message_id: str, timesent: int, sender_id: str, server_id: str, message_content: str):
+
+async def new_message(
+    connection: asyncpg.Connection,
+    message_id: str,
+    timesent: int,
+    sender_id: str,
+    server_id: str,
+    message_content: str,
+):
     logger.info("Attempting to add a new message to DB")
     try:
-        await connection.execute(f"INSERT INTO MESSAGES VALUES('{message_id}', '{timesent}', '{sender_id}', '{server_id}', '{message_content}');")
+        await connection.execute(
+            f"INSERT INTO MESSAGES VALUES('{message_id}', '{timesent}', '{sender_id}', '{server_id}', '{message_content}');"
+        )
         logger.info("Successfully added a new message to DB")
     except Exception as e:
         logger.error("Exception occured while trying to add a message to database")
         logger.error(e)
 
+
 async def get_messages(connection: asyncpg.Connection, server_id: str):
     logger.info("Attempting to get messages from DB")
     try:
-        messages = await connection.fetch(f"SELECT * FROM MESSAGES WHERE server_id='{server_id}'")
+        messages = await connection.fetch(
+            f"SELECT * FROM MESSAGES WHERE server_id='{server_id}'"
+        )
         logger.info("Successfully retrieved messages from DB")
         return messages
     except Exception as e:
