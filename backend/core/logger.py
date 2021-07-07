8d029f76a5ee1bcf4ee5d82da821c4a2bf903e4b
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2021-07-07 16:55:20 -0400

    Refactor logger.py: see extended desc (#59)
    * Refactor logger.py: see extended desc
    
    1. remove redundancy
    2. refine type annotations
    3. add logical spacing
    
    * Fix kron's docstring lint errors
    
    * completely annihilate redundancy
    
    So, it has been agreed that we'll condense all four normal log functions into one, leaving the error log because that accepts different parameters.
    
    * add the missing def keyword

--- backend/core/logger.py
+++ backend/core/logger.py
@@ -1,8 +1,9 @@
-from typing import Optional
 from datetime import datetime
-import traceback
-from sys import stdout
 import os
+from sys import stdout
+import traceback
+from typing import Literal, Optional
+
 import aiofiles
 
 COLORS = {
@@ -19,31 +20,34 @@ COLORS = {
 class Logger:
     """
     Custom logging class.
-    :param mode: String declaring the mode of logging used. Can be hybrid for output to stdout and file, file for output to file, or stdout to stdout.
-    :param filename: Optional string declaring the filename of the log file, if logging type is hybrid or file. Do note that logs will be in the same directory as the file that calls the logging class.
-    :param COLORS: Optional dictionary containing a set of ANSI escape codes to use as COLORS. Requires the following keys: end, error, info, warning and timestamp.
+
+    :param mode: String declaring the mode of logging used.
+    Can be hybrid for output to stdout and file, file for output to file, or stdout to stdout.
+    :param filename: Optional string declaring the filename of the log file, if logging type is hybrid or file.
+    Do note that logs will be in the same directory as the file that calls the logging class.
+    :param COLORS: Optional dictionary containing a set of ANSI escape codes to use as COLORS.
+    Requires the following keys: end, error, info, warning and timestamp.
     """
 
     def __init__(
         self,
-        mode: Optional[str] = "hybrid",
+        mode: Literal["hybrid", "file", "stdout"] = "hybrid",
         filename: Optional[str] = None,
         colored: bool = True,
     ):
-        invalidcount = 0
-        for element in ["hybrid", "file", "stdout"]:
-            if mode != element:
-                invalidcount += 1
-            else:
-                continue
-        if invalidcount == 3:
+        if mode not in ("hybrid", "file", "stdout"):
             raise AttributeError("mode must be hybrid, stdout or file.")
+
         self.colored = colored
         self.loggingtype = mode
+
         if filename:
-            if not os.path.exists(f"catcord_logs/{filename}"):
-                with open(f"catcord_logs/{filename}", "w") as f:
+            directory = f"catcord_logs/{filename}"
+
+            if not os.path.exists(directory):
+                with open(f"{directory}", "w") as f:
                     pass
+
         self.filename = f"catcord_logs/{filename}"
 
     def __enter__(self):
@@ -53,70 +57,49 @@ class Logger:
         if isinstance(exc_type, Exception):
             self.error(content="Exception occured while logging: ")
             self.error(content=exc_value)
+
         del self
 
-    async def _log(self, logtype: str = "info", body: Optional[str] = None):
+    async def _log(self, logtype: str = "INFO", body: Optional[str] = None):
         """
         :param type: String declaring the logging type. Reccomended values are info, warning, and error.
         :param content: String containing the log content.
         """
+        colored_log = (
+            f"{logtype} | {COLORS.get('timestamp')}"
+            f"{datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')}"
+            f"{COLORS.get('end')} | {body}\n"
+        )
+        normal_log = f"{logtype} | {datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')} | {body}\n"
+        log = colored_log if self.colored else normal_log
+
         if self.loggingtype == "file":
             async with aiofiles.open(self.filename, mode="a+") as file:
                 print(body)
-                if self.colored:
-                    await file.write(
-                        f"{logtype} | {COLORS.get('timestamp')}{datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')}{COLORS.get('end')} | {body}\n"
-                    )
-                else:
-                    await file.write(
-                        f"{logtype} | {datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')} | {body}\n"
-                    )
+                await file.write(log)
+
         elif self.loggingtype == "stdout":
-            if self.colored:
-                stdout.write(
-                    f"{logtype} | {COLORS.get('timestamp')}{datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')}{COLORS.get('end')} | {body}\n"
-                )
-            else:
-                stdout.write(
-                    f"{logtype} | {datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')} | {body}\n"
-                )
-        else:
-            if self.colored:
-                async with aiofiles.open(self.filename, "a+") as file:
-                    await file.write(
-                        f"{logtype} | {COLORS.get('timestamp')}{datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')}{COLORS.get('end')} | {body}\n"
-                    )
-                stdout.write(
-                    f"{logtype} | {COLORS.get('timestamp')}{datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')}{COLORS.get('end')} | {body}\n"
-                )
-            else:
-                async with aiofiles.open(self.filename, "a+") as file:
-                    await file.write(
-                        f"{logtype} | {datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')} | {body}\n"
-                    )
-                stdout.write(
-                    f"{logtype} | {datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S UTC')} | {body}\n"
-                )
+            stdout.write(log)
 
-    async def info(self, content: str = None):
-        if self.colored:
-            await self._log(
-                logtype=f"{COLORS.get('info')}INFO{COLORS.get('end')}",
-                body=f"{content}",
-            )
         else:
-            await self._log(logtype="INFO", body=f"{content}")
+            async with aiofiles.open(self.filename, "a+") as file:
+                file.write(log)
+            stdout.write(log)
 
-    async def warning(self, content: str = None):
-        if self.colored:
-            await self._log(
-                logtype=f"{COLORS.get('warning')}WARNING{COLORS.get('end')}",
-                body=f"{content}",
-            )
-        else:
-            await self._log(logtype="WARNING", body=f"{content}")
+    async def log(
+        self,
+        log_mode: Literal["info", "warning", "debug", "success"],
+        content: Optional[str] = None,
+    ):
+        await self._log(
+            logtype="{1}{0}{2}".format(
+                log_mode.upper(),
+                *((COLORS.get(log_mode), COLORS.get('end')) if self.colored else ("", "")),
+            ),
+            body=f"{content}",
+        )
 
-    async def error(self, content: str = None, exc: Optional[bool] = False):
+    async def error_log(self, content: Optional[str] = None, exc: bool = False):
         if exc:
             if self.colored:
                 content += f"\n{COLORS.get('error')}{traceback.format_exc()}"
@@ -129,27 +112,3 @@ class Logger:
             )
         else:
             await self._log(logtype="ERROR", body=f"{content}")
-
-    async def debug(
-        self,
-        content: str = None,
-    ):
-        if self.colored:
-            await self._log(
-                logtype=f"{COLORS.get('debug')}DEBUG{COLORS.get('end')}",
-                body=f"{content}",
-            )
-        else:
-            await self._log(logtype="ERROR", body=f"{content}")
-
-    async def success(
-        self,
-        content: str = None,
-    ):
-        if self.colored:
-            await self._log(
-                logtype=f"{COLORS.get('success')}SUCCESS{COLORS.get('end')}",
-                body=f"{content}",
-            )
-        else:
-            await self._log(logtype="SUCCESS", body=f"{content}")
