51e84e2afbac3563f374b1b94d9cbfdc3543a70a
DawnOfMidnight <78233879+dawnofmidnight@users.noreply.github.com>
2021-07-23 15:07:50 -0400

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -98,7 +98,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "/",
-          editUrl: "https://github.com/cat-dev-group/docs/tree/main/docs/",
+          editUrl: "https://github.com/cat-dev-group/catcord/blob/main/docs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
